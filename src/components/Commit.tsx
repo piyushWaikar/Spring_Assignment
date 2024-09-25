5b97f6187807f60d0c571e0b837ec375d3777e26
Piyush W <hanumantw99@gmail.com>
2024-09-25 15:58:59 +0530

    commit

--- src/components/Commit.tsx
+++ src/components/Commit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Commit = () => {
   const [open, setOpen] = useState(false);
--- src/components/Upgrade.tsx
+++ src/components/Upgrade.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const Upgrade = () => {
   const [open, setOpen] = useState(false);
